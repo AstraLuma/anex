519376ea6ea3f9f14c40baf96bcecfd9023367cf
Jamie Bliss <astronouth7303@gmail.com>
2015-06-16 16:07:01 -0400

    Basic Node model

--- anex/server/leafpile/models.py
+++ anex/server/leafpile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Node(models.Model):
+    id = models.CharField(max_length=128, primary_key=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    source = models.TextField()
