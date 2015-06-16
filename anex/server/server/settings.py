37d2ef1158a97a0994945799feda927475913bc7
Jamie Bliss <astronouth7303@gmail.com>
2015-06-16 16:08:20 -0400

    Update settings

--- anex/server/server/settings.py
+++ anex/server/server/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'anex.server.leafpile',
 )
 
 MIDDLEWARE_CLASSES = (
